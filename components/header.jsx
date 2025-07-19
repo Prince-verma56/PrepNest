594e38d73e4fe76e44b76eabac5cbe3f71dbae48
Prince Verma <pvcodingz96@gmail.com>
2025-07-19 18:34:56 +0530

    some updates

--- components/header.jsx
+++ components/header.jsx
@@ -33,10 +33,10 @@ export default async function Header() {
       <nav className="container mx-auto px-4 h-16 flex items-center justify-between">
         <Link href="/">
           <Image
-            src={"/logo2.png"}
+            src={"/logo4.png"}
             alt="Sensai Logo"
-            width={200}
-            height={60}
+            width={80}
+            height={120}
             className="h-12 py-1 w-auto object-contain"
           />
         </Link>
--- components/hero.jsx
+++ components/hero.jsx
@@ -31,13 +31,10 @@ const HeroSection = () => {
       <div className="space-y-6 text-center">
         <div className="space-y-6 mx-auto">
           <h1 className="text-5xl font-bold md:text-6xl lg:text-7xl xl:text-8xl gradient-title animate-gradient">
-            Your AI Career Coach for
-            <br />
-            Professional Success
+            Career Growth Made <br /> Easy With AI
           </h1>
           <p className="mx-auto max-w-[600px] text-muted-foreground md:text-xl">
-            Advance your career with personalized guidance, interview prep, and
-            AI-powered tools for job success.
+            Prepare for success with AI-driven coaching, interview support, and career growth tools.
           </p>
         </div>
         <div className="flex justify-center space-x-4">
@@ -46,7 +43,7 @@ const HeroSection = () => {
               Get Started
             </Button>
           </Link>
-          <Link href="https://www.youtube.com/roadsidecoder">
+          <Link href="/dashboard">
             <Button size="lg" variant="outline" className="px-8">
               Watch Demo
             </Button>
@@ -55,7 +52,7 @@ const HeroSection = () => {
         <div className="hero-image-wrapper mt-5 md:mt-0">
           <div ref={imageRef} className="hero-image">
             <Image
-              src="/banner.jpeg"
+              src="/Banner-update1.png"
               width={1280}
               height={720}
               alt="Dashboard Preview"
